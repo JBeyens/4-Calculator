7f5690e0c021525e5fe445cf5d3a5bc1080e2867
MacCrimmon <ben.vandevorst@hotmail.com>
2017-10-24 20:19:54 +0200

    Controller updated

--- Calculator2/src/controller/CalculatorController.java
+++ Calculator2/src/controller/CalculatorController.java
@@ -1,5 +1,8 @@
 package controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import view.MainView;
 
 public class CalculatorController {
@@ -7,5 +10,16 @@ public class CalculatorController {
 	
 	public CalculatorController(){
 		view = new MainView();
+	}
+	
+	public void startController(){
+		view.setVisible(true);
+		view.addActionListener(new StartExerciseListener());
+	}
+	
+	private class StartExerciseListener implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			
+		}
 	}
 }

0167ee16a3e89778e272939bfdeeb7daf1e084aa
JBeyens <jefbeyens@gmail.com>
2017-10-24 18:32:54 +0200

    CalculationTest - Calculator approached as static class

--- Calculator2/src/testing/CalculationsTest.java
+++ Calculator2/src/testing/CalculationsTest.java
@@ -16,46 +16,44 @@ import businesscomponent.Calculator;
 
 
 public class CalculationsTest {
-	private Calculator calculator;
 	private double testGetal;
 	private double accuracyResult;
 	
 	@Before
 	public void setUp(){
-		this.calculator = new Calculator();
 		testGetal = 100;
 		accuracyResult = 0.0;
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
 	public void test_division_deler0_wordt_niet_aanvaard() throws Exception {
-		calculator.doCalculation(testGetal, 0, Calculator.DIV);
+		Calculator.doCalculation(testGetal, 0, Calculator.DIV);
 	}
 	
 	@Test
 	public void test_division_geldig_parameter_wordt_aanvaard() throws Exception{
-		double testWaarde = calculator.doCalculation(testGetal, 5, Calculator.DIV);
+		double testWaarde = Calculator.doCalculation(testGetal, 5, Calculator.DIV);
 		
 		assertEquals(20, testWaarde, accuracyResult);
 	}
 	
 	@Test
 	public void test_multiplication_geldig_parameter_wordt_aanvaard() throws Exception{
-		double testWaarde = calculator.doCalculation(testGetal, 5, Calculator.MULT);
+		double testWaarde = Calculator.doCalculation(testGetal, 5, Calculator.MULT);
 		
 		assertEquals(500, testWaarde, accuracyResult);
 	}
 	
 	@Test
 	public void test_substraction_geldig_parameter_wordt_aanvaard() throws Exception{
-		double testWaarde = calculator.doCalculation(testGetal, 5, Calculator.SUB);
+		double testWaarde = Calculator.doCalculation(testGetal, 5, Calculator.SUB);
 		
 		assertEquals(95, testWaarde, accuracyResult);
 	}
 	
 	@Test
 	public void test_addition_geldig_parameter_wordt_aanvaard() throws Exception{
-		double testWaarde = calculator.doCalculation(testGetal, 5, Calculator.ADD);
+		double testWaarde = Calculator.doCalculation(testGetal, 5, Calculator.ADD);
 		
 		assertEquals(105, testWaarde, accuracyResult);
 	}	

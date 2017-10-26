05cb92962a7676d7f11558dabe086dd827cbb8d0
MacCrimmon <ben.vandevorst@hotmail.com>
2017-10-26 15:03:56 +0200

    MainView updated

--- Calculator2/src/view/MainView.java
+++ Calculator2/src/view/MainView.java
@@ -11,6 +11,10 @@ import javax.swing.SpringLayout;
 import java.awt.Color;
 
 public class MainView extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JLabel labelNrOfQuestions;
 	private JLabel labelMinNumber;
 	private JLabel labelMAxNumber;

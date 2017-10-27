f46d36856ee68962c4f1342f1bcf02afa76507a7
JBeyens <jefbeyens@gmail.com>
2017-10-27 18:38:46 +0200

    Exercise - partially written method to round string

--- Calculator2/src/model/exercises/factory/Exercise.java
+++ Calculator2/src/model/exercises/factory/Exercise.java
@@ -57,6 +57,28 @@ public abstract class Exercise {
 		return operation; }
 	
 	/**
+	 * @param input: a double which will be shown to the user
+	 * @param nrDecimals: amount of decimals which should be visible to the user
+	 * @return string: cleand up string with requested amount of decimals to show to the user */
+	private String cleanUp(double input, int nrDecimals) {
+		// Convert to double which should have no decimals
+		double resultAsDouble = input *Math.pow(10.0, nrDecimals);
+		// Round to nearest whole number and convert to int
+		String resultAsString = Integer.valueOf((int) Math.round(resultAsDouble)).toString();
+		
+		// Throw away trailing zeros 
+		// (e.g.:  resultAsString = "100"  and nrDecimals = 2    =>   resultAsString = "1" and nrDecimals = 0)
+		int stringLength = resultAsString.length();
+		while (stringLength > 1 && nrDecimals > 0 && resultAsString.endsWith("0")) {
+			resultAsString = resultAsString.substring(0, stringLength -1);
+			stringLength--;
+			nrDecimals--;			
+		}
+		
+		// TODO: Finish result..........
+		return String.valueOf(input);
+	}
+	/**
 	 * Generates the string to display the exercise */
 	@Override
 	public String toString() {

8b667e97ec0f61864a890f603cd341e4ec95c0ea
MacCrimmon <ben.vandevorst@hotmail.com>
2017-10-27 04:24:27 +0200

    remoeved unused package

--- Calculator2/src/view/MainView.java
+++ Calculator2/src/view/MainView.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
-import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;

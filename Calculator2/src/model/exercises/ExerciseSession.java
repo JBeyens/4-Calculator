592779b0f08fb53f84efeb2f0de1c58eaf4d792c
JBeyens <jefbeyens@gmail.com>
2017-10-28 15:37:10 +0200

    ExerciseManager changed to ExerciseSessoin

---
+++ Calculator2/src/model/exercises/ExerciseSession.java
@@ -0,0 +1,77 @@
+package model.exercises;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Random;
+
+import model.exercises.factory.Exercise;
+import model.exercises.factory.ExerciseFactory;
+
+/**
+ * @Autor: Jef Beyens & Ben Vandevorst
+ * @Date: 16/10/2016
+ * @Project: Calculator
+ * @Purpose: manage exercises
+ *  --> The purpose of this class is to manage exercises. 
+ */
+public class ExerciseSession {
+	// PRIVATE MEMBERS
+	private ArrayList<Exercise> exerciseList;
+	private int exerciseCounter;
+	private boolean isRunning;
+	private LocalDateTime startTime;
+	private LocalDateTime endTime;
+	
+	
+	// CONSTRUCTOR
+	/** Initializes an exercise session
+	 * @param random    An instance of the Random class.
+	 * @param settings  exerciseSettings that must be respected in the session.
+	 **/
+	public ExerciseSession(Random random, ExerciseSettings settings) {
+		exerciseList = new ArrayList<Exercise>();
+		
+		for (int i = 0; i < settings.getNrOfExercises(); i++) {
+			exerciseList.add(ExerciseFactory.getExercise(random, settings));
+		}
+		isRunning = true;
+		startTime = LocalDateTime.now();		
+		exerciseCounter = 0;
+	}
+	
+	
+	// PUBLIC METHODS
+	/**
+	 * Returns the next exercise in the list and increases the exerciseCounter.
+	 * @param none		 The exercises are already generated based on the ExerciseSettings
+	 * @return Exercise  The next exercise in the list. Returns null if there are no exercises, or if there are no more exercises
+	 **/
+	public Exercise getNextExercise() {
+		if (exerciseCounter >= exerciseList.size())
+			return null;
+		return exerciseList.get(exerciseCounter++);
+	} 
+	
+	/**
+	 * Composes a string with statistics from the exercise session (Score, time, etc)
+	 * @return String    A description of the results
+	 **/
+	public String getEndResult() {
+		// Time spent:
+		if (isRunning) {
+			isRunning = false;
+			endTime = LocalDateTime.now();
+		}
+					
+		// TODO: compose string
+		return "TODO: compose string";
+	} 
+	
+	/**
+	 * Returns the number of exercises in this session
+	 * @return int    The number of exercises in this session
+	 **/
+	public int getNumberOfExercises() {
+		return exerciseList.size() + 1;
+	} 
+}

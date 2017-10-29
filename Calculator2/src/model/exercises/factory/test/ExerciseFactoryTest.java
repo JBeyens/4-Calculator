91dbabc761314adcad7bbfcc84d191574ff5c248
JBeyens <jefbeyens@gmail.com>
2017-10-29 19:43:31 +0100

    ExerciseFactoryTest: Test methods created

--- Calculator2/src/model/exercises/factory/test/ExerciseFactoryTest.java
+++ Calculator2/src/model/exercises/factory/test/ExerciseFactoryTest.java
@@ -7,7 +7,11 @@ import org.junit.Test;
 
 import model.exercises.ExerciseSettings;
 import model.exercises.factory.Exercise;
+import model.exercises.factory.ExerciseAdd;
 import model.exercises.factory.ExerciseDiv;
+import model.exercises.factory.ExerciseFactory;
+import model.exercises.factory.ExerciseMult;
+import model.exercises.factory.ExerciseSub;
 import model.exercises.factory.Randomizer;
 
 public class ExerciseFactoryTest {
@@ -25,7 +29,7 @@ public class ExerciseFactoryTest {
 	}
 
 	@Test
-	public void test_GenerateExercises_For_Random_MinRange_And_MaxRange_Expect_Within_Range() {
+	public void test_GetExercise_Should_Return_An_Exercise_With_Correct_Operator() {
 		for (int i = 0; i < 100000; i++) {
 			int rndInt = Randomizer.getRandomNumber(0, boundDecimals);
 			double minRange = Randomizer.getRandomNumber(-1*boundRange, boundRange, rndInt);
@@ -35,14 +39,50 @@ public class ExerciseFactoryTest {
 										    Math.max(minRange, maxRange), 
 										    rndInt, rndInt, operators);
 			
-			exercise = new ExerciseDiv(settings, rndInt);
-
-			assertTrue(exercise.getExerciseNumber() == rndInt); 
-			assertTrue(exercise.getOperand1() >= minRange); 
-			assertTrue(exercise.getOperand2() >= minRange); 
-			assertTrue(exercise.getOperand1() <= maxRange); 
-			assertTrue(exercise.getOperand2() <= maxRange); 
+			settings.setOperators(new String[] {"+"});
+			exercise = ExerciseFactory.getExercise(settings, rndInt);
+			assertTrue(exercise.getOperation() == '+'); 
+			
+			settings.setOperators(new String[] {"-"});
+			exercise = ExerciseFactory.getExercise(settings, rndInt);
+			assertTrue(exercise.getOperation() == '-'); 
+			
+			settings.setOperators(new String[] {"*"});
+			exercise = ExerciseFactory.getExercise(settings, rndInt);
+			assertTrue(exercise.getOperation() == '*'); 
+			
+			settings.setOperators(new String[] {"/"});
+			exercise = ExerciseFactory.getExercise(settings, rndInt);
 			assertTrue(exercise.getOperation() == '/'); 			
+		}
+	}
+
+	@Test
+	public void test_GetExercise_Should_Return_An_Exercise_With_Correct_Type() {
+		for (int i = 0; i < 100000; i++) {
+			int rndInt = Randomizer.getRandomNumber(0, boundDecimals);
+			double minRange = Randomizer.getRandomNumber(-1*boundRange, boundRange, rndInt);
+			double maxRange = Randomizer.getRandomNumber(-1*boundRange, boundRange, rndInt);
+			
+			settings = new ExerciseSettings(Math.min(minRange, maxRange), 
+										    Math.max(minRange, maxRange), 
+										    rndInt, rndInt, operators);
+			
+			settings.setOperators(new String[] {"+"});
+			exercise = ExerciseFactory.getExercise(settings, rndInt);
+			assertTrue(exercise instanceof ExerciseAdd); 
+			
+			settings.setOperators(new String[] {"-"});
+			exercise = ExerciseFactory.getExercise(settings, rndInt);
+			assertTrue(exercise instanceof ExerciseSub); 
+			
+			settings.setOperators(new String[] {"*"});
+			exercise = ExerciseFactory.getExercise(settings, rndInt);
+			assertTrue(exercise instanceof ExerciseMult); 
+			
+			settings.setOperators(new String[] {"/"});
+			exercise = ExerciseFactory.getExercise(settings, rndInt);
+			assertTrue(exercise instanceof ExerciseDiv); 			
 		}
 	}
 }

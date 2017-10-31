31a9275c35854ba6bc6d8c029c7e3bc14cd40759
JBeyens <jefbeyens@gmail.com>
2017-10-31 14:48:25 +0100

    PropertyFileReader - same SetUp & TearDown structure as PropertyManagerTest

--- Calculator2/src/model/properties/test/PropertyFileReaderTest.java
+++ Calculator2/src/model/properties/test/PropertyFileReaderTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertTrue;
 import java.io.File;
 import java.util.Properties;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,20 +14,22 @@ import model.exercisesFactory.Randomizer;
 import model.properties.PropertyFileReader;
 
 public class PropertyFileReaderTest {
+	private File file;
+	private String path = "Random path";
 	
+	//Set up file
 	@Before
-	public void setUp(){ }
-	
-	@Test
-	public void test_Save_Properties_Expect_Same_As_Load_Properties() {
+	public void setUp(){
 		// Ensure file does not exist:
-		String path = "Random path";
-		File file = new File(path);
+		file = new File(path);
 		while (file.exists()) {
 			path = Long.toString(Randomizer.RANDOM.nextLong()) + ".txt";
 			file = new File(path);
-		}			
-		
+		}	
+	}	
+	
+	@Test
+	public void test_Save_Properties_Expect_Same_As_Load_Properties() {
 		// Prepare properties
 		Properties propertiesBefore = new Properties();
 		propertiesBefore.setProperty("Happy times", "I hear you!");
@@ -40,9 +43,12 @@ public class PropertyFileReaderTest {
 		assertTrue(propertiesAfter.containsKey("Happy times"));
 		assertEquals(propertiesAfter.get("Happy times"), "I hear you!");
 		assertTrue(propertiesAfter.containsKey("1 Johny + 1 Johny"));
-		assertEquals(propertiesAfter.get("1 Johny + 1 Johny"), "2 Johnies");	
-		
-		// Clean up file
+		assertEquals(propertiesAfter.get("1 Johny + 1 Johny"), "2 Johnies");
+	}
+	
+	//Clean up file
+	@After
+	public void tearDown(){
 		file.delete();
 	}
 }
